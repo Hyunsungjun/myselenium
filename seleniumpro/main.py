5134d41865d9742fde6171d95863712edafe1d70
jun <bnmva23@hanmail.net>
2020-10-28 00:17:27 +0900

    1.2

--- seleniumpro/main.py
+++ seleniumpro/main.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 browser = webdriver.Chrome(ChromeDriverManager().install())
 browser.get("https://google.com")
 #여기까진 거의 고정이겠군
-
+##
 search_bar = browser.find_element_by_class_name("gLFyf")
 search_bar.send_keys("hello!")
 search_bar.send_keys(Keys.ENTER)
