1197bc5e260c571492858d3e3c308974f2a254ab
jun <bnmva23@hanmail.net>
2020-10-28 00:11:40 +0900

    1.2

---
+++ seleniumpro/main.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Chrome(ChromeDriverManager().install())
+browser.get("https://google.com")
+#여기까진 거의 고정이겠군
+
+search_bar = browser.find_element_by_class_name("gLFyf")
+search_bar.send_keys("hello!")
+search_bar.send_keys(Keys.ENTER)
+
+search_results = browser.find_elements_by_class_name("g")
+
+for search_result in search_results:
+    title = search_result.find_element_by_tag_name("h3")
+    if title:
+        print(title.text)
